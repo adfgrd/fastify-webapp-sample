8abf3161a0a6a12450c114c512df43560de24d6f
yasui <bell.inu.ushi.anaguma.panda@gmail.com>
2025-10-14 09:06:58 +0900

    URLを環境変数から渡すようにしたい

--- e2e/codecept.conf.js
+++ e2e/codecept.conf.js
@@ -1,10 +1,22 @@
 /** @type {CodeceptJS.MainConfig} */
+const { setHeadlessWhen, setCommonPlugins } = require ('@codeceptjs/configure');
+require('dotenv').config();
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+require('dotenv').config();
+
 exports.config = {
   tests: './*_test.js',
   output: './output',
   helpers: {
     Playwright: {
-      url: 'http://localhost',
+      url: process.env.BASE_URL,
       show: true,
       browser: 'chromium'
     }
@@ -14,5 +26,16 @@ exports.config = {
   },
   translation: 'en-US',
   vocabularies: ['./vocabularies.json'],
-  name: 'e2e'
-}
+  name: 'e2e',
+  plugins: {
+    allure: {
+      enabled: true,
+      require: "allure-codeceptjs",
+    },
+    stepByStepReport: {
+      enabled: true,
+      screenshotsForAllureReport: true,
+      deleteSuccessful: false,
+    },
+  },
+};
